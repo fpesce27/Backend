b0edad5a589de186af73d4bc88bffa9cdf290b18
fpesce27 <fpesce@frba.utn.edu.ar>
2022-08-11 16:13:10 -0300

    primer commit

---
+++ src/main/java/com/proyecto/main/repos/RepoProductos.java
@@ -0,0 +1,14 @@
+package com.proyecto.main.repos;
+
+import com.proyecto.main.model.Producto;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RepositoryRestController
+public interface RepoProductos extends PagingAndSortingRepository<Producto, Long> {
+    public Optional<Producto> findById(Long id);
+    public List<Producto> findAll();
+}
